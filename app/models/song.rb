2caa031e190c1931f2d1fabc7eaaac061d0e0f0c
Dusty211 <kylebenz@hotmail.com>
2019-02-04 14:59:22 -0500

    all test pass.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,36 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :genre
+  belongs_to :artist
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if note.strip != ''
+        self.notes.build(content: note)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map &:content
+  end
+
 end
